80c64602564abaa980599e032d828e3a2d75a6f6
Jameson Wilder <jaw@mge.local>
2019-09-18 15:01:05 -0700

    new

--- Python/FileName_Print.py
+++ Python/FileName_Print.py
@@ -1,5 +1,5 @@
 import os
 
-for root, dirs, files in os.walk(r"C:\Users\jaw\Desktop\BP10_BLM_Before_And_After_Photos"):
+for root, dirs, files in os.walk(r"C:\Users\jaw\Desktop\Div01_Photos_Before_and_After"):
     for filename in files:
         print(filename)
