5e295bcda0338312fde53d04b61ca18e34dbeb55
Jameson Wilder <jamesonwilder@gmail.com>
2020-06-22 07:08:41 -0700

    delete

--- Python/EVM Process/PGE_North_Valley/Bridgeville_1101/PGE_Bridgeville_1101_Export_Data.py
+++
@@ -1,8 +0,0 @@
-import arcpy
-
-# Set environment settings
-arcpy.env.workspace = "C:/data/GreenvalleyDB.gdb/Public Buildings"
-
-arcpy.FeatureClassToFeatureClass_conversion("buildings_point", 
-                                            "C:/output/output.gdb", 
-                                            "buildings_point")
